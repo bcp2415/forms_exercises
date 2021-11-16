fdef2bdb0d220616e77a540a2490d2894c95d2aa
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-16 15:10:03 -0600

    finishes form elements, begins validation

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", init);
+
+function init() {
+    const submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener("click", validate);
+}
+
+function validate() {
+    
+}
