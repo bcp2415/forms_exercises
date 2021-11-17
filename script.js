5f3a35ae4a4a8189ec6ec0a7cf73dcabbdb45643
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-17 11:57:18 -0600

    adds beginning of validation for # of boosters.

--- script.js
+++ script.js
@@ -11,5 +11,8 @@ function validateWholeForm() {
 
 function validateBoosters() {
   const boosters = document.getElementById("boosterCount");
-  while (boosters.value < 1 || boosters.value > 10) {}
+  while (boosters.value < 1 || boosters.value > 10) {
+    window.alert("Please enter a number from 1 to 10 for the Number of Rocket Boosters.")
+    boosters.value = 1;
+  };
 }
