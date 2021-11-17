249966a00abbd9fa9a96d9eb2b3caa127ca79088
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-17 09:57:50 -0600

    removed console.logs, as validation now works (framework, not content)

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 window.addEventListener("load", init);
 
 function init() {
-  console.log("init called.");
   const submitButton = document.getElementById("submitButton");
   submitButton.addEventListener("click", validateWholeForm);
 }
 
 function validateWholeForm() {
-  console.log("validateWholeForm called.");
   validateBoosters();
 }
 
 function validateBoosters() {
   const boosters = document.getElementById("boosterCount");
-  console.log(boosters.value);
 }
