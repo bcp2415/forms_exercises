838aece94d7ff57b919680ceef15c9314438f150
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-17 19:32:11 -0600

    validation for rocket booster count now partially working

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 window.addEventListener("load", init);
 
 function init() {
-  const submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", validateWholeForm);
-}
-
-function validateWholeForm() {
-  validateBoosters();
-}
-
-function validateBoosters() {
-  const boosters = document.getElementById("boosterCount");
-<<<<<<< HEAD
-  if (boosters.value < 1 || boosters.value > 10) {
-    window.alert(
-      "Please enter a value for the number of Rocket Boosters from 1 to 10."
-    );
-  }
+  const form = document.getElementById("form");
+  form.addEventListener("submit", function (event) {
+    const boosters = document.getElementById("boosterCount");
+    if (boosters.value < 1 || boosters.value > 10) {
+      window.alert(
+        "Please enter a value for the number of Rocket Boosters from 1 to 10."
+      );
+      event.preventDefault();
+    }
+  });
 }
